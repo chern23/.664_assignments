5cdb2020ccdf5ce2ab114c217c0ffbf347fe55b2
Candace <chapter23@rocketmail.com>
2023-09-18 19:59:41 -0400

    commit exercise 3

---
+++ week_3/pratt_schedule2.py
@@ -0,0 +1,45 @@
+schedule = {
+	"monday" :   ['Human-Information Behavior','Information Professions','Research Mthds/Law Lit','Strategic Leadership','Conservation and Preservation','Info Services & Resources'],
+	"tuesday":   ['Information Arch/Inter Design','Information Professions','Acad Libraries and Scholarly','Info Services & Resources'],
+	"wednesday": ['Info Services & Resources','Usability Theory & Practice','Mgmt of Archives/Sp Collection','Government Info Sources','Library Media Centers','Mgmt of Archives/Sp Collection'],
+	"thursday": ['Information Science Research','Information Professions','Information Professions'],
+}
+
+
+for day in schedule:
+    print(day)
+    for classes in schedule[day]:
+        print(classes)
+
+
+
+#loop through each day in schedule, print the name of the day, and then loop through all the class names and print them out
+
+#output sample:
+
+# ❯ python3 pratt_schedule2.py
+# monday
+# Human-Information Behavior
+# Information Professions
+# ...
+
+
+
+# ATTEMPT 1 - prints days only
+# day = "{'monday'} {'tuesday} {'wednesday} {'thursday'}"
+
+# for day in schedule:
+#           print(schedule)
+
+
+# ATTEMPT 2 - prints schedule but not in right format
+# for day in schedule:
+#     print(day,
+#           schedule[day])
+    
+
+# # ATTEMPT 3 
+# for day in schedule:
+#     print(schedule[day])
+
+
