512b7cde52b0d155606d160f60a8b4928952be07
Candace <chapter23@rocketmail.com>
2023-10-03 08:47:40 -0400

    folder updates commit

--- week_2/week_2.py
+++
@@ -1,8 +0,0 @@
-print("hello world")
-list_of_numbers = [1,2,3,4,5,6]
-list_of_strings = ['Rik', 'Mia', 'Luke']
-anything = [5,"things",True,None,9.12]
-print(anything[0])
-print(anything[3])
-print("length of anything:", len(anything))
-print(anything[len(anything)-1])
