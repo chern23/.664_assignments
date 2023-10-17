dfaa723163c7f67010fdc9ac1497344e274b2317
Candace <chapter23@rocketmail.com>
2023-10-17 13:13:38 -0400

    week 4 retry commit

--- week_4/week_4.py
+++ week_4/week_4.py
@@ -10,12 +10,12 @@ with open('week_4/Artworks.csv') as art_file:
             nationalities_list = nationalities_str.split(' ')
             for nat in nationalities_list:
                if nat_list.get(nat) is None:
-                nat_file = open(f'week_4/res/{nat}.csv', 'w')
+                nat_file = open(f'res/{nat}.csv', 'w')
                 nat_dict_writer = csv.DictWriter(nat_file, processed_csv.fieldnames)
                 nat_dict_writer.writeheader()
                 nat_dict_writer.writerow(artwork)
                 nat_list[nat] = True
                else: 
-                nat_file = open(f'week_4/res/{nat}.csv', 'a')
+                nat_file = open(f'res/{nat}.csv', 'a')
                 nat_dict_writer = csv.DictWriter(nat_file, processed_csv.fieldnames)
                 nat_dict_writer.writerow(artwork)
